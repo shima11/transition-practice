5569ed89cb1866a9a24c4798669f8f8c75735de2
Shima <shima.jin@icloud.com>
2019-06-18 09:40:38 +0900

    wip

--- transition-practice/ViewController.swift
+++ transition-practice/ViewController.swift
@@ -12,9 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.frame = .init(x: 0, y: 0, width: 100, height: 100)
+        button.center = view.center
+        button.setTitle("hoge", for: .normal)
+        view.addSubview(button)
+
+    }
+
+    @objc func didTapButton() {
+
+        let controller = DetailViewController()
+        present(controller, animated: true, completion: nil)
     }
 
 
 }
 
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.frame = .init(x: 0, y: 0, width: 100, height: 100)
+        button.center = view.center
+        button.setTitle("hoge", for: .normal)
+        view.addSubview(button)
+
+    }
+
+    @objc func didTapButton() {
+
+        let controller = DetailViewController()
+        controller.modalPresentationStyle = .overFullScreen
+        present(controller, animated: true, completion: nil)
+        
+    }
+
+}
