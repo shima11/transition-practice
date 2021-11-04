b7f2be13fc38dc5cc0ce929c403633521261697c
Shima <shima.jin@icloud.com>
2021-11-04 20:10:27 +0900

    update

--- Pods/Target Support Files/Pods-transition-practice/Pods-transition-practice-frameworks.sh
+++ Pods/Target Support Files/Pods-transition-practice/Pods-transition-practice-frameworks.sh
@@ -177,11 +177,11 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/EasyPeasy/EasyPeasy.framework"
-  install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer/iOS/RevealServer.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Reveal-SDK/RevealServer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/EasyPeasy/EasyPeasy.framework"
-  install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer/iOS/RevealServer.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Reveal-SDK/RevealServer.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
